7e5ed310ba5dc33a98adef7c438c198defcd25c8
nicolashzmor <nicolashernandezamor@doesrobbiedream.com>
2022-01-17 11:00:26 -0300

    fix(vault): add marked as dependency and cleanup boilerplate

--- apps/vault/src/app/app.module.ts
+++ apps/vault/src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import {AppComponent} from './app.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
